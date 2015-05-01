8a4be5fb2cfa7dfc8a0675f6f629635284d03b2c
Michael Sacks <michaelsacks@gmail.com>
2015-04-30 21:38:08 -0600

    Final Copy

--- FramedArrangement/AppDelegate.m
+++ FramedArrangement/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -19,8 +20,11 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    self.window.rootViewController = [ViewController new];
     
     return YES;
 }
--- FramedArrangement/ViewController.m
+++ FramedArrangement/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIView *redView;
+@property (nonatomic, strong) UIView *greenView;
+@property (nonatomic, strong) UIView *blueView;
+@property (nonatomic, strong) UIView *yellowView;
+
 @end
 
 @implementation ViewController
@@ -17,6 +22,66 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    self.redView = [UIView new];
+    self.redView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:self.redView];
+    
+    self.greenView = [UIView new];
+    self.greenView.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:self.greenView];
+    
+    self.blueView = [UIView new];
+    self.blueView.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:self.blueView];
+    
+    self.yellowView = [UIView new];
+    self.yellowView.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:self.yellowView];
+    
+    [self layoutSquares];
+    //[self layoutHorizontalRectangles];
+    //[self layoutVerticleRectangles];
+    //[self layoutDiagonalSquares];
+}
+
+-(void)layoutSquares{
+    CGFloat square = [[UIScreen mainScreen] bounds].size.width/2.0;
+    
+    self.redView.frame = CGRectMake(0, 25, square, square);
+    self.greenView.frame = CGRectMake(square, 25, square, square);
+    self.blueView.frame = CGRectMake(0, square+25, square, square);
+    self.yellowView.frame = CGRectMake(square, square+25, square, square);
+}
+
+-(void)layoutHorizontalRectangles{
+    CGFloat width = [[UIScreen mainScreen] bounds].size.width;
+    CGFloat height = width/4.0;
+    
+    self.redView.frame = CGRectMake(0, 25, width, height);
+    self.greenView.frame = CGRectMake(0, height + 25, width, height);
+    self.blueView.frame = CGRectMake(0, height*2.0 + 25, width, height);
+    self.yellowView.frame = CGRectMake(0, height*3 + 25, width, height);
+    
+}
+
+-(void)layoutVerticleRectangles{
+    CGFloat height = [[UIScreen mainScreen] bounds].size.width;
+    CGFloat width = height/4.0;
+    
+    self.redView.frame = CGRectMake(0, 25, width, height);
+    self.greenView.frame = CGRectMake(width, 25, width, height);
+    self.blueView.frame = CGRectMake(width*2.0, 25, width, height);
+    self.yellowView.frame = CGRectMake(width*3.0, 25, width, height);
+}
+
+-(void)layoutDiagonalSquares{
+    CGFloat square = [[UIScreen mainScreen] bounds].size.width/4.0;
+    
+    self.redView.frame = CGRectMake(0, 25, square, square);
+    self.greenView.frame = CGRectMake(square, square + 25, square, square);
+    self.blueView.frame = CGRectMake(square * 2.0, square * 2.0 + 25, square, square);
+    self.yellowView.frame = CGRectMake(square * 3.0, square * 3.0 + 25, square, square);
 }
 
 - (void)didReceiveMemoryWarning {
